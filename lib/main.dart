1e2b87c3850478fd3205ea6d0c624669d151879e
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-05-20 01:55:40 +0200

    Add BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app_orm/blocs/bloc/todo_bloc.dart';
 import 'package:todo_app_orm/injection_container.dart' as di;
 import 'package:todo_app_orm/ui/todo_screen.dart';
 
@@ -12,7 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: TodoScreen(),
+      home: BlocProvider(
+        create: (context) => TodoBloc(),
+        child: TodoScreen(),
+      ),
     );
   }
 }
