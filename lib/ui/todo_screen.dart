31a341da865f4413d702fd2123cc458c95441be4
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-05-19 17:29:44 +0200

    Add basic UI for todo screen

---
+++ lib/ui/todo_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class TodoScreen extends StatelessWidget {
+  const TodoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TO DO:'),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() => Column(
+        children: [
+          Flexible(
+            flex: 12,
+            child: _buildTodoList(),
+          ),
+          Flexible(flex: 2, child: _buildTextInput()),
+          Flexible(
+            flex: 1,
+            child: Container(),
+          ),
+          Flexible(
+            flex: 2,
+            child: _buildSubmitButton(),
+          ),
+        ],
+      );
+
+  Widget _buildTodoList() => ListView.builder(
+        itemCount: 6,
+        itemBuilder: (context, index) => Container(),
+      );
+
+  Widget _buildTextInput() => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(),
+            ),
+          ),
+        ),
+      );
+
+  Widget _buildSubmitButton() => ElevatedButton(
+        child: Text('Submit'),
+        onPressed: () {},
+      );
+}
